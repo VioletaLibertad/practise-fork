a47a4eb6632daca6ea5cd14e146bee1ed66b9774
Violeta <morada.libertad@gmail.com>
2018-05-22 11:52:57 -0400

    Primer commit

---
+++ assets/JS/app.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var btn = document.getElementById("button-change");
+    var txt = document.getElementById("paragraph");
+    var btnDos = document.getElementById("magic");
+    var txtDos = document.getElementById("text");
+
+    btn.addEventListener("click", function() {
+      txt.innerHTML = "<h2>Git y Github te amamos</h2>";
+    })
+
+    btnDos.addEventListener("click", function(){
+        //txtDos.innerHTML = " ";
+        txtDos.classList.add("disappear");
+    })
+}
